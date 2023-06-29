f01454413f1cc26ff7ee916ca7b83e3d002c4647
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-06-29 10:00:26 -0400

    Updated JSON P Streaming API to Jakarta EE.

---
+++ ch04_src/jsonpstreaming/src/main/java/com/ensode/jakartaeebook/jsonpstreaming/Customer.java
@@ -0,0 +1,63 @@
+package com.ensode.jakartaeebook.jsonpstreaming;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+
+@Named
+@RequestScoped
+public class Customer implements Serializable {
+
+  private String firstName;
+  private String lastName;
+  private String email;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  @Override
+  public String toString() {
+    String localFirstName = firstName;
+    String localLastName = lastName;
+    String localEmail = email;
+
+    if (localEmail == null) {
+      localEmail = "";
+    }
+
+    if (localFirstName == null) {
+      localFirstName = "";
+    }
+    if (localLastName == null) {
+      localLastName = "";
+    }
+
+    String toString;
+    toString = "firstName = " + localFirstName + "\n";
+    toString += "lastName = " + localLastName + "\n";
+    toString += "email = " + localEmail;
+
+    return toString;
+  }
+}
