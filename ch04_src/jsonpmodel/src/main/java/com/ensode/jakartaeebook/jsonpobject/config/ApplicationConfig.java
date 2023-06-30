43d6f114035466ae5f2099663c2d7875df60337a
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-06-30 10:41:06 -0400

    Updated package name.

--- ch04_src/jsonpmodel/src/main/java/com/ensode/jakartaeebook/jsonpobject/config/ApplicationConfig.java
+++ ch04_src/jsonpmodel/src/main/java/com/ensode/jakartaeebook/jsonpobject/config/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package com.ensode.jakartaee8book.jsonpobject.config;
+package com.ensode.jakartaeebook.jsonpobject.config;
 
 @jakarta.ws.rs.ApplicationPath("resources")
 public class ApplicationConfig extends jakarta.ws.rs.core.Application {
