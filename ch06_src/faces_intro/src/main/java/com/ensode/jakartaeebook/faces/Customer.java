c776e62184bc2031de818eb2f51826eddf0f05a7
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-07-12 06:57:25 -0400

    Added Jakarta Faces example.

---
+++ ch06_src/faces_intro/src/main/java/com/ensode/jakartaeebook/faces/Customer.java
@@ -0,0 +1,37 @@
+package com.ensode.jakartaeebook.faces;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class Customer {
+
+  private String firstName;
+  private String lastName;
+  private String email;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
