874d2c30286c69a28cb7bfd870feebadd45bd120
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-10-11 11:19:59 -0400

    Added an example for HTTP authentication, using a database identity store.

---
+++ ch10_src/httpauthdbidentitystore/src/main/java/com/ensode/jakartaeebook/httpauthdbidentitystore/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.ensode.jakartaeebook.httpauthdbidentitystore;
+
+import jakarta.annotation.PreDestroy;
+import jakarta.annotation.Resource;
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Initialized;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import jakarta.security.enterprise.identitystore.Pbkdf2PasswordHash;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+
+@DataSourceDefinition(name = "java:global/jdbc/userauthdbDatasource",
+        className = "org.h2.jdbcx.JdbcDataSource",
+        url = "jdbc:h2:tcp://127.0.1.1:9092/mem:userauthdb",
+        user = "sa",
+        password = "")
+@ApplicationScoped
+public class SecurityConfig {
+
+  @Resource(lookup = "java:global/jdbc/userauthdbDatasource")
+  private DataSource dataSource;
+
+  @Inject
+  private Pbkdf2PasswordHash passwordHash;
+
+  public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
+    try {
+      Map<String, String> parameters = new HashMap<>();
+      parameters.put("Pbkdf2PasswordHash.Iterations", "3072");
+      parameters.put("Pbkdf2PasswordHash.Algorithm", "PBKDF2WithHmacSHA512");
+      parameters.put("Pbkdf2PasswordHash.SaltSizeBytes", "64");
+      passwordHash.initialize(parameters);
+
+      executeUpdate(dataSource, "INSERT INTO USERS (USER_ID,USERNAME,FIRST_NAME, LAST_NAME,PASSWORD) VALUES (1,'alice','Alice','Jones','"
+              + passwordHash.generate("aaa".toCharArray()) + "')");
+      executeUpdate(dataSource, "INSERT INTO USERS (USER_ID,USERNAME,FIRST_NAME, LAST_NAME,PASSWORD) VALUES (2,'bob','Robert','Smith','"
+              + passwordHash.generate("bbb".toCharArray()) + "')");
+
+      executeUpdate(dataSource, "INSERT INTO USER_GROUPS (USER_ID,GROUP_ID) VALUES (1,1)");
+      executeUpdate(dataSource, "INSERT INTO USER_GROUPS (USER_ID,GROUP_ID) VALUES (2,2)");
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+  }
+
+  private void executeUpdate(DataSource dataSource, String query) {
+    try (Connection connection = dataSource.getConnection()) {
+      try (PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.executeUpdate();
+      }
+    } catch (SQLException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  @PreDestroy
+  public void destroy() {
+    try {
+      executeUpdate(dataSource, "DROP TABLE IF EXISTS USERS");
+      executeUpdate(dataSource, "DROP TABLE IF EXISTS GROUPS");
+      executeUpdate(dataSource, "DROP TABLE IF EXISTS USER_GROUPS");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+}
