a1451718f4d975ec9dfa44783798dfdd8271fe5c
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-11-02 10:28:03 -0400

    Added example illustrating developing a client for Session Enterprise Beans.

---
+++ ch12_src/simplesessionbeanclient/src/main/java/com/ensode/jakartaeebook/SessionBeanClient.java
@@ -0,0 +1,21 @@
+package com.ensode.jakartaeebook;
+
+import jakarta.ejb.EJB;
+import javax.naming.NamingException;
+
+public class SessionBeanClient {
+
+    @EJB
+    private static SimpleSession simpleSession;
+
+    private void invokeSessionBeanMethods() throws NamingException {
+        System.out.println(simpleSession.getMessage());
+
+        System.out.println("\nSimpleSession is of type: "
+                + simpleSession.getClass().getName());
+    }
+
+    public static void main(String[] args) throws NamingException {
+        new SessionBeanClient().invokeSessionBeanMethods();
+    }
+}
