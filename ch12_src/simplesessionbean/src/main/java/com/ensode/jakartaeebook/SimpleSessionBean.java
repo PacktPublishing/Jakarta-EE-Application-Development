e569b6ca2b82920c2cd9a0084a914e298bc919f0
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-11-03 09:05:27 -0400

    Added missing @Override annotation.

--- ch12_src/simplesessionbean/src/main/java/com/ensode/jakartaeebook/SimpleSessionBean.java
+++ ch12_src/simplesessionbean/src/main/java/com/ensode/jakartaeebook/SimpleSessionBean.java
@@ -7,6 +7,7 @@ public class SimpleSessionBean implements SimpleSession{
 
     private final String message = "If you don't see this, it didn't work!";
 
+    @Override
     public String getMessage() {
         return message;
     }
