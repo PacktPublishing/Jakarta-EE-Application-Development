1a89fe3d3e9fe703c073f01041f47bcdb4877f2e
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-11-08 13:54:30 -0500

    Updated calendar based timer example.

--- ch12_src/calendarbasedtimer/src/main/java/com/ensode/javaee8book/calendarbasedtimer/CalendarBasedTimerJebExampleBean.java
+++ ch12_src/calendarbasedtimer/src/main/java/com/ensode/javaee8book/calendarbasedtimer/CalendarBasedTimerJebExampleBean.java
@@ -1,17 +1,15 @@
 package com.ensode.javaee8book.calendarbasedtimer;
 
-import jakarta.ejb.LocalBean;
 import jakarta.ejb.Schedule;
 import jakarta.ejb.Stateless;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Stateless
-@LocalBean
-public class CalendarBasedTimerEjbExampleBean {
+public class CalendarBasedTimerJebExampleBean {
 
   private static Logger logger = Logger.getLogger(
-      CalendarBasedTimerEjbExampleBean.class.getName());
+      CalendarBasedTimerJebExampleBean.class.getName());
 
   @Schedule(hour = "20", minute = "10")
   public void logMessage() {
