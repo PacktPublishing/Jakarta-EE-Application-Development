fc533ff8bc8d4c3000fd26d68e10530b07433987
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-11-10 07:10:58 -0500

    Updated enterprise bean timer client example.

--- ch12_src/jebtimerclient/src/main/java/com/ensode/jakartaeebook/Client.java
+++ ch12_src/jebtimerclient/src/main/java/com/ensode/jakartaeebook/Client.java
@@ -2,31 +2,27 @@ package com.ensode.jakartaeebook;
 
 import jakarta.ejb.EJB;
 
-public class Client
-{
-  @EJB
-  private static JebTimerExample ejbTimerExample;
+public class Client {
 
-  public static void main(String[] args)
-  {
-    try
-    {
+  @EJB
+  private static JebTimerExample jebTimerExample;
+
+  public static void main(String[] args) {
+    try {
       System.out.println("Starting timer 1...");
-      ejbTimerExample.startTimer("Timer 1");
+      jebTimerExample.startTimer("Timer 1");
       System.out.println("Sleeping for 2 seconds...");
       Thread.sleep(2000);
       System.out.println("Starting timer 2...");
-      ejbTimerExample.startTimer("Timer 2");
+      jebTimerExample.startTimer("Timer 2");
       System.out.println("Sleeping for 30 seconds...");
       Thread.sleep(30000);
       System.out.println("Stopping timer 1...");
-      ejbTimerExample.stopTimer("Timer 1");
+      jebTimerExample.stopTimer("Timer 1");
       System.out.println("Stopping timer 2...");
-      ejbTimerExample.stopTimer("Timer 2");
+      jebTimerExample.stopTimer("Timer 2");
       System.out.println("Done.");
-    }
-    catch (InterruptedException e)
-    {
+    } catch (InterruptedException e) {
       e.printStackTrace();
     }
   }
