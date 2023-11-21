d0b5c068fa38762d4048fb331644ac82824be16e
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-11-21 09:39:46 -0500

    Added @JMSDestinationDefinition annotation and added web.xml.

--- ch13_src/queuebrowser/src/main/java/com/ensode/jakartaeebook/queuebrowser/MessageQueueBrowser.java
+++ ch13_src/queuebrowser/src/main/java/com/ensode/jakartaeebook/queuebrowser/MessageQueueBrowser.java
@@ -5,6 +5,7 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSContext;
+import jakarta.jms.JMSDestinationDefinition;
 import jakarta.jms.JMSException;
 import jakarta.jms.Queue;
 import jakarta.jms.QueueBrowser;
@@ -13,46 +14,50 @@ import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@JMSDestinationDefinition(
+    name = "java:global/queue/JakartaEEBookQueue",
+    interfaceName = "jakarta.jms.Queue"
+)
 
 @Named
 @RequestScoped
 public class MessageQueueBrowser {
 
-    @Resource
-    private ConnectionFactory connectionFactory;
-    @Resource(mappedName = "jms/JavaEE8BookQueue")
-    private Queue queue;
-    private static final Logger LOG = Logger.getLogger(MessageQueueBrowser.class.getName());
+  @Resource
+  private ConnectionFactory connectionFactory;
+  @Resource(mappedName = "java:global/queue/JakartaEEBookQueue")
+  private Queue queue;
+  private static final Logger LOG = Logger.getLogger(MessageQueueBrowser.class.getName());
 
-    public void browseMessages() {
-        try {
-            Enumeration messageEnumeration;
-            TextMessage textMessage;
-            JMSContext jmsContext = connectionFactory.createContext();
-            QueueBrowser browser = jmsContext.createBrowser(queue);
+  public void browseMessages() {
+    try {
+      Enumeration messageEnumeration;
+      TextMessage textMessage;
+      JMSContext jmsContext = connectionFactory.createContext();
+      QueueBrowser browser = jmsContext.createBrowser(queue);
 
-            messageEnumeration = browser.getEnumeration();
+      messageEnumeration = browser.getEnumeration();
 
-            if (messageEnumeration != null) {
-                if (!messageEnumeration.hasMoreElements()) {
-                    LOG.log(Level.INFO, "There are no messages "
-                            + "in the queue.");
-                } else {
-                    LOG.log(Level.INFO,
-                            "The following messages are in the queue:");
-                    while (messageEnumeration.hasMoreElements()) {
-                        textMessage = (TextMessage) messageEnumeration.
-                                nextElement();
-                        LOG.log(Level.INFO, textMessage.getText());
-                    }
-                }
-            }
-        } catch (JMSException e) {
-            LOG.log(Level.SEVERE, "JMS Exception caught", e);
+      if (messageEnumeration != null) {
+        if (!messageEnumeration.hasMoreElements()) {
+          LOG.log(Level.INFO, "There are no messages "
+              + "in the queue.");
+        } else {
+          LOG.log(Level.INFO,
+              "The following messages are in the queue:");
+          while (messageEnumeration.hasMoreElements()) {
+            textMessage = (TextMessage) messageEnumeration.
+                nextElement();
+            LOG.log(Level.INFO, textMessage.getText());
+          }
         }
+      }
+    } catch (JMSException e) {
+      LOG.log(Level.SEVERE, "JMS Exception caught", e);
     }
+  }
 
-    public static void main(String[] args) {
-        new MessageQueueBrowser().browseMessages();
-    }
+  public static void main(String[] args) {
+    new MessageQueueBrowser().browseMessages();
+  }
 }
