f482975ca07e1cc20446c9f420871d66b4294dcd
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-11-21 10:13:10 -0500

    Added @JMSDestinationDefinition annotation.

--- ch13_src/pubsubproducer/src/main/java/com/ensode/jakartaeebook/pubsubproducer/MessageSender.java
+++ ch13_src/pubsubproducer/src/main/java/com/ensode/jakartaeebook/pubsubproducer/MessageSender.java
@@ -5,35 +5,41 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSContext;
+import jakarta.jms.JMSDestinationDefinition;
 import jakarta.jms.JMSProducer;
 import jakarta.jms.Topic;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@JMSDestinationDefinition(
+    name = "java:global/topic/JakartaEEBookTopic",
+    interfaceName = "jakarta.jms.Topic"
+)
+
 @Named
 @RequestScoped
 public class MessageSender {
 
-    @Resource
-    private ConnectionFactory connectionFactory;
-    @Resource(mappedName = "jms/JavaEE8BookTopic")
-    private Topic topic;
-    private static final Logger LOG = Logger.getLogger(MessageSender.class.getName());
+  @Resource
+  private ConnectionFactory connectionFactory;
+  @Resource(mappedName = "java:global/topic/JakartaEEBookTopic")
+  private Topic topic;
+  private static final Logger LOG = Logger.getLogger(MessageSender.class.getName());
 
-    public void produceMessages() {
-        JMSContext jmsContext = connectionFactory.createContext();
-        JMSProducer jmsProducer = jmsContext.createProducer();
+  public void produceMessages() {
+    JMSContext jmsContext = connectionFactory.createContext();
+    JMSProducer jmsProducer = jmsContext.createProducer();
 
-        String msg1 = "Testing, 1, 2, 3. Can you hear me?";
-        String msg2 = "Do you copy?";
-        String msg3 = "Good bye!";
+    String msg1 = "Testing, 1, 2, 3. Can you hear me?";
+    String msg2 = "Do you copy?";
+    String msg3 = "Good bye!";
 
-        LOG.log(Level.INFO, "Sending the following message: {0}", msg1);
-        jmsProducer.send(topic, msg1);
-        LOG.log(Level.INFO, "Sending the following message: {0}", msg2);
-        jmsProducer.send(topic, msg2);
-        LOG.log(Level.INFO, "Sending the following message: {0}", msg3);
-        jmsProducer.send(topic, msg3);
+    LOG.log(Level.INFO, "Sending the following message: {0}", msg1);
+    jmsProducer.send(topic, msg1);
+    LOG.log(Level.INFO, "Sending the following message: {0}", msg2);
+    jmsProducer.send(topic, msg2);
+    LOG.log(Level.INFO, "Sending the following message: {0}", msg3);
+    jmsProducer.send(topic, msg3);
 
-    }
+  }
 }
