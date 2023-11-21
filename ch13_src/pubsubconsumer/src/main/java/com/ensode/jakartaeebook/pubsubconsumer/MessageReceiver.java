9c5c127c2edbd1b61a4fcfe7e0a15f7749f03d09
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-11-21 10:26:11 -0500

    Added @JMSDestinationDefinition annotation and updated web.xml.

--- ch13_src/pubsubconsumer/src/main/java/com/ensode/jakartaeebook/pubsubconsumer/MessageReceiver.java
+++ ch13_src/pubsubconsumer/src/main/java/com/ensode/jakartaeebook/pubsubconsumer/MessageReceiver.java
@@ -6,38 +6,43 @@ import jakarta.inject.Named;
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSConsumer;
 import jakarta.jms.JMSContext;
+import jakarta.jms.JMSDestinationDefinition;
 import jakarta.jms.Topic;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+@JMSDestinationDefinition(
+    name = "java:global/topic/JakartaEEBookTopic",
+    interfaceName = "jakarta.jms.Topic"
+)
 
 @Named
 @RequestScoped
 public class MessageReceiver {
 
-    @Resource
-    private ConnectionFactory connectionFactory;
-    @Resource(mappedName = "jms/JavaEE8BookTopic")
-    private Topic topic;
-    private static final Logger LOG = Logger.getLogger(MessageReceiver.class.getName());
+  @Resource
+  private ConnectionFactory connectionFactory;
+  @Resource(mappedName = "java:global/topic/JakartaEEBookTopic")
+  private Topic topic;
+  private static final Logger LOG = Logger.getLogger(MessageReceiver.class.getName());
 
-    public void receiveMessages() {
-        String message;
-        boolean goodByeReceived = false;
+  public void receiveMessages() {
+    String message;
+    boolean goodByeReceived = false;
 
-        JMSContext jmsContext = connectionFactory.createContext();
-        JMSConsumer jMSConsumer = jmsContext.createConsumer(topic);
+    JMSContext jmsContext = connectionFactory.createContext();
+    JMSConsumer jMSConsumer = jmsContext.createConsumer(topic);
 
-        LOG.log(Level.INFO, "Waiting for messages...");
-        while (!goodByeReceived) {
-            message = jMSConsumer.receiveBody(String.class);
+    LOG.log(Level.INFO, "Waiting for messages...");
+    while (!goodByeReceived) {
+      message = jMSConsumer.receiveBody(String.class);
 
-            if (message != null) {
-                System.out.print("Received the following message: ");
-                LOG.log(Level.INFO, message);
-                if (message.equals("Good bye!")) {
-                    goodByeReceived = true;
-                }
-            }
+      if (message != null) {
+        LOG.log(Level.INFO, "Received the following message: {0}", message);
+        if (message.equals("Good bye!")) {
+          goodByeReceived = true;
         }
+      }
     }
+  }
 }
