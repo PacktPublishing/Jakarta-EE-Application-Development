85f54e31fed11f9875a1ef1fa3c5e10cff2f4293
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-12-05 08:47:22 -0500

    Reformatted source.

--- ch12_src/messagedriven/src/main/java/com/ensode/jakartaeebook/ExampleMessageDrivenBean.java
+++ ch12_src/messagedriven/src/main/java/com/ensode/jakartaeebook/ExampleMessageDrivenBean.java
@@ -11,20 +11,21 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @JMSDestinationDefinition(
-  name="java:global/queue/JakartaEEBookQueue",
-  interfaceName="jakarta.jms.Queue",
-  destinationName = "JakartaEEBookQueue"
+    name = "java:global/queue/JakartaEEBookQueue",
+    interfaceName = "jakarta.jms.Queue",
+    destinationName = "JakartaEEBookQueue"
 )
 
 @MessageDriven(activationConfig = {
   @ActivationConfigProperty(propertyName = "destinationLookup",
       propertyValue = "java:global/queue/JakartaEEBookQueue"),
-  @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "jakarta.jms.Queue")
+  @ActivationConfigProperty(propertyName = "destinationType",
+      propertyValue = "jakarta.jms.Queue")
 })
 public class ExampleMessageDrivenBean implements MessageListener {
 
-  private static final Logger LOG = Logger.getLogger(ExampleMessageDrivenBean.class.getName());
-
+  private static final Logger LOG = Logger.getLogger(
+      ExampleMessageDrivenBean.class.getName());
 
   public void onMessage(Message message) {
     TextMessage textMessage = (TextMessage) message;
