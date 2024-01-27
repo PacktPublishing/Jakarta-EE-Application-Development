d82681972a0fac214465c7b7b87cc9ce8493dba1
David R. Heffelfinger <dheffelfinger@ensode.com>
2024-01-27 09:17:12 -0500

    Simplifying code.

--- ch15_src/jakartaeealltogether/src/main/java/com/ensode/jakartaeealltogether/faces/controller/UsStateController.java
+++ ch15_src/jakartaeealltogether/src/main/java/com/ensode/jakartaeealltogether/faces/controller/UsStateController.java
@@ -1,222 +1,30 @@
 package com.ensode.jakartaeealltogether.faces.controller;
 
 import com.ensode.jakartaeealltogether.dao.UsStateDao;
-import com.ensode.jakartaeealltogether.dao.exceptions.NonexistentEntityException;
-import com.ensode.jakartaeealltogether.entity.UsState;
-import com.ensode.jakartaeealltogether.faces.converter.UsStateConverter;
 import com.ensode.jakartaeealltogether.faces.util.JsfUtil;
-import com.ensode.jakartaeealltogether.faces.util.PagingInfo;
-import jakarta.annotation.Resource;
 import jakarta.ejb.EJB;
 import jakarta.enterprise.context.SessionScoped;
-import jakarta.faces.FacesException;
-import jakarta.faces.component.UIComponent;
-import jakarta.faces.context.FacesContext;
-import jakarta.faces.convert.Converter;
 import jakarta.faces.model.SelectItem;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceUnit;
-import jakarta.transaction.UserTransaction;
 import java.io.Serializable;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
 
 @Named("usState")
 @SessionScoped
 public class UsStateController implements Serializable {
 
-  public UsStateController() {
-    pagingInfo = new PagingInfo();
-    converter = new UsStateConverter();
-  }
-  private UsState usState = null;
-  private List<UsState> usStateItems = null;
   @EJB
   private UsStateDao dao;
-  private UsStateConverter converter = null;
-  private PagingInfo pagingInfo = null;
-  @Resource
-  private UserTransaction utx = null;
   @PersistenceUnit(unitName = "customerPersistenceUnit")
   private EntityManagerFactory emf = null;
 
-  public PagingInfo getPagingInfo() {
-    if (pagingInfo.getItemCount() == -1) {
-      pagingInfo.setItemCount(getDao().getUsStateCount());
-    }
-    return pagingInfo;
+  public SelectItem[] getUsStateItemsAvailableSelectOne() {
+    return JsfUtil.getSelectItems(dao.findUsStateEntities(), true);
   }
 
   public UsStateDao getDao() {
-
     return dao;
-  }
-
-  public SelectItem[] getUsStateItemsAvailableSelectMany() {
-    return JsfUtil.getSelectItems(getDao().findUsStateEntities(), false);
-  }
-
-  public SelectItem[] getUsStateItemsAvailableSelectOne() {
-    return JsfUtil.getSelectItems(getDao().findUsStateEntities(), true);
-  }
-
-  public UsState getUsState() {
-    if (usState == null) {
-      usState = (UsState) JsfUtil.getObjectFromRequestParameter("jsfcrud.currentUsState", converter, null);
-    }
-    if (usState == null) {
-      usState = new UsState();
-    }
-    return usState;
-  }
-
-  public String listSetup() {
-    reset(true);
-    return "usState_list";
-  }
-
-  public String createSetup() {
-    reset(false);
-    usState = new UsState();
-    return "usState_create";
-  }
-
-  public String create() {
-    try {
-      getDao().create(usState);
-      JsfUtil.addSuccessMessage("UsState was successfully created.");
-    } catch (Exception e) {
-      JsfUtil.ensureAddErrorMessage(e, "A persistence error occurred.");
-      return null;
-    }
-    return listSetup();
-  }
-
-  public String detailSetup() {
-    return scalarSetup("usState_detail");
-  }
-
-  public String editSetup() {
-    return scalarSetup("usState_edit");
-  }
-
-  private String scalarSetup(String destination) {
-    reset(false);
-    usState = (UsState) JsfUtil.getObjectFromRequestParameter("jsfcrud.currentUsState", converter, null);
-    if (usState == null) {
-      String requestUsStateString = JsfUtil.getRequestParameter("jsfcrud.currentUsState");
-      JsfUtil.addErrorMessage("The usState with id " + requestUsStateString + " no longer exists.");
-      return relatedOrListOutcome();
-    }
-    return destination;
-  }
-
-  public String edit() {
-    String usStateString = converter.getAsString(FacesContext.getCurrentInstance(), null, usState);
-    String currentUsStateString = JsfUtil.getRequestParameter("jsfcrud.currentUsState");
-    if (usStateString == null || usStateString.length() == 0 || !usStateString.equals(currentUsStateString)) {
-      String outcome = editSetup();
-      if ("usState_edit".equals(outcome)) {
-        JsfUtil.addErrorMessage("Could not edit usState. Try again.");
-      }
-      return outcome;
-    }
-    try {
-      getDao().edit(usState);
-      JsfUtil.addSuccessMessage("UsState was successfully updated.");
-    } catch (NonexistentEntityException ne) {
-      JsfUtil.addErrorMessage(ne.getLocalizedMessage());
-      return listSetup();
-    } catch (Exception e) {
-      JsfUtil.ensureAddErrorMessage(e, "A persistence error occurred.");
-      return null;
-    }
-    return detailSetup();
-  }
-
-  public String destroy() {
-    String idAsString = JsfUtil.getRequestParameter("jsfcrud.currentUsState");
-    Integer id = Integer.valueOf(idAsString);
-    try {
-      getDao().destroy(id);
-      JsfUtil.addSuccessMessage("UsState was successfully deleted.");
-    } catch (NonexistentEntityException ne) {
-      JsfUtil.addErrorMessage(ne.getLocalizedMessage());
-      return relatedOrListOutcome();
-    } catch (Exception e) {
-      JsfUtil.ensureAddErrorMessage(e, "A persistence error occurred.");
-      return null;
-    }
-    return relatedOrListOutcome();
-  }
-
-  private String relatedOrListOutcome() {
-    String relatedControllerOutcome = relatedControllerOutcome();
-    if (relatedControllerOutcome != null) {
-      return relatedControllerOutcome;
-    }
-    return listSetup();
-  }
-
-  public List<UsState> getUsStateItems() {
-    if (usStateItems == null) {
-      getPagingInfo();
-      usStateItems = getDao().findUsStateEntities(pagingInfo.getBatchSize(), pagingInfo.getFirstItem());
-    }
-    return usStateItems;
-  }
-
-  public String next() {
-    reset(false);
-    getPagingInfo().nextPage();
-    return "usState_list";
-  }
-
-  public String prev() {
-    reset(false);
-    getPagingInfo().previousPage();
-    return "usState_list";
-  }
-
-  private String relatedControllerOutcome() {
-    String relatedControllerString = JsfUtil.getRequestParameter("jsfcrud.relatedController");
-    String relatedControllerTypeString = JsfUtil.getRequestParameter("jsfcrud.relatedControllerType");
-    if (relatedControllerString != null && relatedControllerTypeString != null) {
-      FacesContext context = FacesContext.getCurrentInstance();
-      Object relatedController = context.getApplication().getELResolver().getValue(context.getELContext(), null, relatedControllerString);
-      try {
-        Class<?> relatedControllerType = Class.forName(relatedControllerTypeString);
-        Method detailSetupMethod = relatedControllerType.getMethod("detailSetup");
-        return (String) detailSetupMethod.invoke(relatedController);
-      } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-        throw new FacesException(e);
-      }
-    }
-    return null;
-  }
-
-  private void reset(boolean resetFirstItem) {
-    usState = null;
-    usStateItems = null;
-    pagingInfo.setItemCount(-1);
-    if (resetFirstItem) {
-      pagingInfo.setFirstItem(0);
-    }
-  }
-
-  public void validateCreate(FacesContext facesContext, UIComponent component, Object value) {
-    UsState newUsState = new UsState();
-    String newUsStateString = converter.getAsString(FacesContext.getCurrentInstance(), null, newUsState);
-    String usStateString = converter.getAsString(FacesContext.getCurrentInstance(), null, usState);
-    if (!newUsStateString.equals(usStateString)) {
-      createSetup();
-    }
-  }
-
-  public Converter getConverter() {
-    return converter;
   }
 
 }
