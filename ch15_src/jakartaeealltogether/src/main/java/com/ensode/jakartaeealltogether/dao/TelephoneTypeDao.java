fefdf2730034c48e135164b0b24ed5a6094dfb76
David R. Heffelfinger <dheffelfinger@ensode.com>
2024-01-27 09:30:23 -0500

    Simplifying code.

--- ch15_src/jakartaeealltogether/src/main/java/com/ensode/jakartaeealltogether/dao/TelephoneTypeDao.java
+++ ch15_src/jakartaeealltogether/src/main/java/com/ensode/jakartaeealltogether/dao/TelephoneTypeDao.java
@@ -1,145 +1,24 @@
 package com.ensode.jakartaeealltogether.dao;
 
-import com.ensode.jakartaeealltogether.dao.exceptions.NonexistentEntityException;
-import com.ensode.jakartaeealltogether.dao.exceptions.PreexistingEntityException;
-import com.ensode.jakartaeealltogether.dao.exceptions.RollbackFailureException;
-import com.ensode.jakartaeealltogether.entity.Telephone;
 import com.ensode.jakartaeealltogether.entity.TelephoneType;
 import jakarta.annotation.Resource;
 import jakarta.ejb.EJBContext;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
 public class TelephoneTypeDao implements Serializable {
 
-  @Resource
-  private EJBContext ejbContext;
-
   @PersistenceContext
   private EntityManager em;
 
-  public void create(TelephoneType telephoneType) throws PreexistingEntityException, RollbackFailureException, Exception {
-    if (telephoneType.getTelephoneList() == null) {
-      telephoneType.setTelephoneList(new ArrayList<>());
-    }
-    try {
-
-      List<Telephone> attachedTelephoneList = new ArrayList<>();
-      for (Telephone telephoneListTelephoneToAttach : telephoneType.getTelephoneList()) {
-        telephoneListTelephoneToAttach = em.getReference(telephoneListTelephoneToAttach.getClass(), telephoneListTelephoneToAttach.getTelephoneId());
-        attachedTelephoneList.add(telephoneListTelephoneToAttach);
-      }
-      telephoneType.setTelephoneList(attachedTelephoneList);
-      em.persist(telephoneType);
-      for (Telephone telephoneListTelephone : telephoneType.getTelephoneList()) {
-        TelephoneType oldTelephoneTypeIdOfTelephoneListTelephone = telephoneListTelephone.getTelephoneTypeId();
-        telephoneListTelephone.setTelephoneTypeId(telephoneType);
-        telephoneListTelephone = em.merge(telephoneListTelephone);
-        if (oldTelephoneTypeIdOfTelephoneListTelephone != null) {
-          oldTelephoneTypeIdOfTelephoneListTelephone.getTelephoneList().remove(telephoneListTelephone);
-          oldTelephoneTypeIdOfTelephoneListTelephone = em.merge(oldTelephoneTypeIdOfTelephoneListTelephone);
-        }
-      }
-    } catch (Exception ex) {
-      try {
-        ejbContext.setRollbackOnly();
-      } catch (Exception re) {
-        throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
-      }
-      if (findTelephoneType(telephoneType.getTelephoneTypeId()) != null) {
-        throw new PreexistingEntityException("TelephoneType " + telephoneType + " already exists.", ex);
-      }
-      throw ex;
-    }
-  }
-
-  public void edit(TelephoneType telephoneType) throws NonexistentEntityException, RollbackFailureException, Exception {
-    try {
-      TelephoneType persistentTelephoneType = em.find(TelephoneType.class, telephoneType.getTelephoneTypeId());
-      List<Telephone> telephoneListOld = persistentTelephoneType.getTelephoneList();
-      List<Telephone> telephoneListNew = telephoneType.getTelephoneList();
-      List<Telephone> attachedTelephoneListNew = new ArrayList<>();
-      for (Telephone telephoneListNewTelephoneToAttach : telephoneListNew) {
-        telephoneListNewTelephoneToAttach = em.getReference(telephoneListNewTelephoneToAttach.getClass(), telephoneListNewTelephoneToAttach.getTelephoneId());
-        attachedTelephoneListNew.add(telephoneListNewTelephoneToAttach);
-      }
-      telephoneListNew = attachedTelephoneListNew;
-      telephoneType.setTelephoneList(telephoneListNew);
-      telephoneType = em.merge(telephoneType);
-      for (Telephone telephoneListOldTelephone : telephoneListOld) {
-        if (!telephoneListNew.contains(telephoneListOldTelephone)) {
-          telephoneListOldTelephone.setTelephoneTypeId(null);
-          telephoneListOldTelephone = em.merge(telephoneListOldTelephone);
-        }
-      }
-      for (Telephone telephoneListNewTelephone : telephoneListNew) {
-        if (!telephoneListOld.contains(telephoneListNewTelephone)) {
-          TelephoneType oldTelephoneTypeIdOfTelephoneListNewTelephone = telephoneListNewTelephone.getTelephoneTypeId();
-          telephoneListNewTelephone.setTelephoneTypeId(telephoneType);
-          telephoneListNewTelephone = em.merge(telephoneListNewTelephone);
-          if (oldTelephoneTypeIdOfTelephoneListNewTelephone != null && !oldTelephoneTypeIdOfTelephoneListNewTelephone.equals(telephoneType)) {
-            oldTelephoneTypeIdOfTelephoneListNewTelephone.getTelephoneList().remove(telephoneListNewTelephone);
-            oldTelephoneTypeIdOfTelephoneListNewTelephone = em.merge(oldTelephoneTypeIdOfTelephoneListNewTelephone);
-          }
-        }
-      }
-    } catch (Exception ex) {
-      try {
-        ejbContext.setRollbackOnly();
-      } catch (Exception re) {
-        throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
-      }
-      String msg = ex.getLocalizedMessage();
-      if (msg == null || msg.length() == 0) {
-        Integer id = telephoneType.getTelephoneTypeId();
-        if (findTelephoneType(id) == null) {
-          throw new NonexistentEntityException("The telephoneType with id " + id + " no longer exists.");
-        }
-      }
-      throw ex;
-    }
-  }
-
-  public void destroy(Integer id) throws NonexistentEntityException, RollbackFailureException, Exception {
-    try {
-      TelephoneType telephoneType;
-      try {
-        telephoneType = em.getReference(TelephoneType.class, id);
-        telephoneType.getTelephoneTypeId();
-      } catch (EntityNotFoundException enfe) {
-        throw new NonexistentEntityException("The telephoneType with id " + id + " no longer exists.", enfe);
-      }
-      List<Telephone> telephoneList = telephoneType.getTelephoneList();
-      for (Telephone telephoneListTelephone : telephoneList) {
-        telephoneListTelephone.setTelephoneTypeId(null);
-        telephoneListTelephone = em.merge(telephoneListTelephone);
-      }
-      em.remove(telephoneType);
-    } catch (Exception ex) {
-      try {
-        ejbContext.setRollbackOnly();
-      } catch (Exception re) {
-        throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
-      }
-      throw ex;
-    }
-  }
-
   public List<TelephoneType> findTelephoneTypeEntities() {
     return findTelephoneTypeEntities(true, -1, -1);
-  }
-
-  public List<TelephoneType> findTelephoneTypeEntities(int maxResults, int firstResult) {
-    return findTelephoneTypeEntities(false, maxResults, firstResult);
   }
 
   private List<TelephoneType> findTelephoneTypeEntities(boolean all, int maxResults, int firstResult) {
@@ -155,14 +34,6 @@ public class TelephoneTypeDao implements Serializable {
 
   public TelephoneType findTelephoneType(Integer id) {
     return em.find(TelephoneType.class, id);
-  }
-
-  public int getTelephoneTypeCount() {
-    CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
-    Root<TelephoneType> rt = cq.from(TelephoneType.class);
-    cq.select(em.getCriteriaBuilder().count(rt));
-    Query q = em.createQuery(cq);
-    return ((Long) q.getSingleResult()).intValue();
   }
 
 }
--- ch15_src/jakartaeealltogether/src/main/java/com/ensode/jakartaeealltogether/dao/UsStateDao.java
+++ ch15_src/jakartaeealltogether/src/main/java/com/ensode/jakartaeealltogether/dao/UsStateDao.java
@@ -1,151 +1,22 @@
 package com.ensode.jakartaeealltogether.dao;
 
-import com.ensode.jakartaeealltogether.dao.exceptions.NonexistentEntityException;
-import com.ensode.jakartaeealltogether.dao.exceptions.PreexistingEntityException;
-import com.ensode.jakartaeealltogether.dao.exceptions.RollbackFailureException;
-import com.ensode.jakartaeealltogether.entity.Address;
 import com.ensode.jakartaeealltogether.entity.UsState;
-import jakarta.annotation.Resource;
-import jakarta.ejb.EJBContext;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
 public class UsStateDao implements Serializable {
 
-  @Resource
-  private EJBContext ejbContext;
-
   @PersistenceContext
   private EntityManager em;
 
-  public void create(UsState usState) throws PreexistingEntityException, RollbackFailureException, Exception {
-    if (usState.getAddressList() == null) {
-      usState.setAddressList(new ArrayList<>());
-    }
-    try {
-
-      List<Address> attachedAddressList = new ArrayList<>();
-      for (Address addressListAddressToAttach : usState.getAddressList()) {
-        addressListAddressToAttach = em.getReference(addressListAddressToAttach.getClass(), addressListAddressToAttach.getAddressId());
-        attachedAddressList.add(addressListAddressToAttach);
-      }
-      usState.setAddressList(attachedAddressList);
-      em.persist(usState);
-      for (Address addressListAddress : usState.getAddressList()) {
-        UsState oldUsStateIdOfAddressListAddress = addressListAddress.getUsStateId();
-        addressListAddress.setUsStateId(usState);
-        addressListAddress = em.merge(addressListAddress);
-        if (oldUsStateIdOfAddressListAddress != null) {
-          oldUsStateIdOfAddressListAddress.getAddressList().remove(addressListAddress);
-          oldUsStateIdOfAddressListAddress = em.merge(oldUsStateIdOfAddressListAddress);
-        }
-      }
-    } catch (Exception ex) {
-      try {
-        ejbContext.setRollbackOnly();
-      } catch (Exception re) {
-        throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
-      }
-      if (findUsState(usState.getUsStateId()) != null) {
-        throw new PreexistingEntityException("UsState " + usState + " already exists.", ex);
-      }
-      throw ex;
-    } finally {
-      if (em != null) {
-        em.close();
-      }
-    }
-  }
-
-  public void edit(UsState usState) throws NonexistentEntityException, RollbackFailureException, Exception {
-    try {
-
-      UsState persistentUsState = em.find(UsState.class, usState.getUsStateId());
-      List<Address> addressListOld = persistentUsState.getAddressList();
-      List<Address> addressListNew = usState.getAddressList();
-      List<Address> attachedAddressListNew = new ArrayList<>();
-      for (Address addressListNewAddressToAttach : addressListNew) {
-        addressListNewAddressToAttach = em.getReference(addressListNewAddressToAttach.getClass(), addressListNewAddressToAttach.getAddressId());
-        attachedAddressListNew.add(addressListNewAddressToAttach);
-      }
-      addressListNew = attachedAddressListNew;
-      usState.setAddressList(addressListNew);
-      usState = em.merge(usState);
-      for (Address addressListOldAddress : addressListOld) {
-        if (!addressListNew.contains(addressListOldAddress)) {
-          addressListOldAddress.setUsStateId(null);
-          addressListOldAddress = em.merge(addressListOldAddress);
-        }
-      }
-      for (Address addressListNewAddress : addressListNew) {
-        if (!addressListOld.contains(addressListNewAddress)) {
-          UsState oldUsStateIdOfAddressListNewAddress = addressListNewAddress.getUsStateId();
-          addressListNewAddress.setUsStateId(usState);
-          addressListNewAddress = em.merge(addressListNewAddress);
-          if (oldUsStateIdOfAddressListNewAddress != null && !oldUsStateIdOfAddressListNewAddress.equals(usState)) {
-            oldUsStateIdOfAddressListNewAddress.getAddressList().remove(addressListNewAddress);
-            oldUsStateIdOfAddressListNewAddress = em.merge(oldUsStateIdOfAddressListNewAddress);
-          }
-        }
-      }
-    } catch (Exception ex) {
-      try {
-        ejbContext.setRollbackOnly();
-      } catch (Exception re) {
-        throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
-      }
-      String msg = ex.getLocalizedMessage();
-      if (msg == null || msg.length() == 0) {
-        Integer id = usState.getUsStateId();
-        if (findUsState(id) == null) {
-          throw new NonexistentEntityException("The usState with id " + id + " no longer exists.");
-        }
-      }
-      throw ex;
-    }
-  }
-
-  public void destroy(Integer id) throws NonexistentEntityException, RollbackFailureException, Exception {
-    try {
-      UsState usState;
-      try {
-        usState = em.getReference(UsState.class, id);
-        usState.getUsStateId();
-      } catch (EntityNotFoundException enfe) {
-        throw new NonexistentEntityException("The usState with id " + id + " no longer exists.", enfe);
-      }
-      List<Address> addressList = usState.getAddressList();
-      for (Address addressListAddress : addressList) {
-        addressListAddress.setUsStateId(null);
-        addressListAddress = em.merge(addressListAddress);
-      }
-      em.remove(usState);
-
-    } catch (Exception ex) {
-      try {
-        ejbContext.setRollbackOnly();
-      } catch (Exception re) {
-        throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
-      }
-      throw ex;
-    }
-  }
-
   public List<UsState> findUsStateEntities() {
     return findUsStateEntities(true, -1, -1);
-  }
-
-  public List<UsState> findUsStateEntities(int maxResults, int firstResult) {
-    return findUsStateEntities(false, maxResults, firstResult);
   }
 
   private List<UsState> findUsStateEntities(boolean all, int maxResults, int firstResult) {
@@ -161,14 +32,6 @@ public class UsStateDao implements Serializable {
 
   public UsState findUsState(Integer id) {
     return em.find(UsState.class, id);
-  }
-
-  public int getUsStateCount() {
-    CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
-    Root<UsState> rt = cq.from(UsState.class);
-    cq.select(em.getCriteriaBuilder().count(rt));
-    Query q = em.createQuery(cq);
-    return ((Long) q.getSingleResult()).intValue();
   }
 
 }
