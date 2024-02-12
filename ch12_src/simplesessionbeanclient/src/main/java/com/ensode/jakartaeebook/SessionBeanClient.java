a3e8602f7e2cefe9cec4d5d986d93dd9afc614d8
David R. Heffelfinger <dheffelfinger@ensode.com>
2024-02-12 07:49:36 -0500

    Updated name of the jar file generated by the Maven Assembly Plugin.

--- ch12_src/simplesessionbeanclient/src/main/java/com/ensode/jakartaeebook/SessionBeanClient.java
+++ ch12_src/simplesessionbeanclient/src/main/java/com/ensode/jakartaeebook/SessionBeanClient.java
@@ -5,17 +5,17 @@ import javax.naming.NamingException;
 
 public class SessionBeanClient {
 
-    @EJB
-    private static SimpleSession simpleSession;
+  @EJB
+  private static SimpleSession simpleSession;
 
-    private void invokeSessionBeanMethods() throws NamingException {
-        System.out.println(simpleSession.getMessage());
+  private void invokeSessionBeanMethods() throws NamingException {
+    System.out.println(simpleSession.getMessage());
 
-        System.out.println("\nSimpleSession is of type: "
-                + simpleSession.getClass().getName());
-    }
+    System.out.println("\nSimpleSession is of type: "
+        + simpleSession.getClass().getName());
+  }
 
-    public static void main(String[] args) throws NamingException {
-        new SessionBeanClient().invokeSessionBeanMethods();
-    }
+  public static void main(String[] args) throws NamingException {
+    new SessionBeanClient().invokeSessionBeanMethods();
+  }
 }
